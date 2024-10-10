8e9b38282ac76bf624194b641ff4be1262798f81
Selina227 <selinac33@nycstudents.net>
2024-10-09 23:46:00 -0400

    e

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 //Assignment 1
 const students = [
-  {
-    name: "Alice",
-    age: 20,
-    grade: 85,
-  },
-  {
-    name: "Bob",
-    age: 22,
-    grade: 90,
-  },
-  {
-    name: "Charlie",
-    age: 21,
-    grade: 78,
-  },
-  {
-    name: "Diana",
-    age: 23,
-    grade: 95,
-  },
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
 ];
 
 //Part 1
@@ -49,17 +33,43 @@ const products = [
 //Part 1
 products.forEach((products) => console.log(products.name, products.price));
 
-const updatedProducts = [];
-products.forEach((product) => {
-  if (
-    updatedProducts.includes(
-      product.name,
-      product.price,
-      product.category,
-      product.rating
-    )
-  ) {
-    updatedProducts.push(product.price * 1.1);
+const updatedProducts = products.map((products) => {
+  return {
+    ...products,
+    price: products.price * 1.1,
+  };
+});
+updatedProducts.forEach((products) =>
+  console.log(products.name, products.price)
+);
+
+const categories = [];
+products.forEach((products) => {
+  if (!categories.includes(products.category)) {
+    categories.push(products.category);
   }
 });
-console.log(updatedProducts);
+console.log("Unique Categories:", categories);
+
+//Part 2
+const electronics = products.filter(
+  (products) => products.category === "Electronics"
+);
+console.log("Electronics:", electronics);
+
+const expensiveProducts = products.filter((products) => products.price > 300);
+console.log("Expensive Products:", expensiveProducts);
+
+const highlyRatedProducts = products.filter(
+  (products) => products.rating >= 4.5
+);
+console.log("Highly Rated Products:", highlyRatedProducts);
+
+//Part 3
+highlyRatedProducts.forEach((products) => console.log(products.name));
+
+const affordableElectronics = products.filter(
+  (products) => products.category === "Electronics",
+  products.price < 1000
+);
+affordableElectronics.forEach((products) => console.log(products.name));
